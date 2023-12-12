2d8c01daaccb384291418954500a4505e201ef75
dmitry <shklyaev1dmitry@gmail.com>
2023-12-12 23:07:19 +0400

    Исправлено подключение к базе данных

--- master_add.php
+++ master_add.php
@@ -20,12 +20,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Подготовка и выполнение запроса
     $stmt = $mysqli->prepare("INSERT INTO users (login, password, id_role, isTop, name) VALUES (?, ?, ?, ?, ?)");
 
+    // Проверка успешности выполнения запроса
+    if ($stmt) {
+        $stmt->bind_param("ssiss", $login, $password, $id_role, $isTop, $name);
+        $stmt->execute();
 
+        if ($stmt->affected_rows > 0) {
+            echo "Регистрация прошла успешно!" . "<br><a class='btn btn-primary' href='admin_page.php'>Возврат на сайт</a>";
+        } else {
+            echo "Ошибка при регистрации: " . $stmt->error;
+        }
 
         // Закрытие соединения
         $stmt->close();
     } else {
-        echo "Ошибка при подготовке запроса: " . $mysqli->error;
+
     }
 
     $mysqli->close();
